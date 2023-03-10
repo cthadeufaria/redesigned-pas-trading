1c7518bc2b8f23aeed6cfe6bc62c917d31cfc96d
cthadeufaria <crls.thadeu@gmail.com>
2023-03-10 21:32:23 +0000

    1st pid simulation 2

--- src/libraries/operations/discrete_time.py
+++ src/libraries/operations/discrete_time.py
@@ -10,22 +10,29 @@ mk = 806.91204500 # nº of shares hold of BUSD from api
 vpk = pk*nak + mk
 vref = 1.1*vpk
 
+print('vpk :', vpk)
+print('vref :', vref)
+
 def price_estimator(pk):
     delta = random.uniform(-0.05, 0.05)
     return pk*(delta + 1)
 
 # initial simulation parameters
-uk = 1
 tstop = 30
 ts = 1
 uk_prev = 0
 e_prev = 0
 
-def controller(ts, vpk, vref, uk_prev, e_prev):
+def controller(ts, vpk, vref, uk_prev, e_prev, pk):
     kp = 0.5
-    ti = 5
+    ti = 0.5
     e = vref - vpk
     uk = uk_prev + kp*(e - e_prev) + (kp/ti)*ts*e
+    alfa = np.sign(uk)
+    nam = np.absolute(uk)
+    if nam*pk > vpk:
+        uk = (vpk/pk)*alfa
+
     return uk, e
 
 data = []
@@ -33,7 +40,7 @@ data.append(vpk)
 n = int(tstop/ts)
 
 for k in range(n):
-    u, e = controller(ts, vpk, vref, uk_prev, e_prev)
+    uk, e = controller(ts, vpk, vref, uk_prev, e_prev, pk)
     uk_prev = uk
     e_prev = e
     pk1 = price_estimator(pk)
@@ -41,12 +48,10 @@ for k in range(n):
     vpk1 = vpk + dpk*nak + dpk*uk - r*pk*np.absolute(uk)
     vpk = vpk1
     pk = pk1
-    alfa = np.sign(uk)
-    nam = np.absolute(uk)
 
     data.append(vpk)
 
-    print('pk1: ', pk1, 'dpk: ', dpk, 'vpk1: ', vpk1)
+    print('pk1: ', pk1, 'dpk: ', dpk, 'vpk1: ', vpk1, 'uk :', uk)
 
 t = np.arange(0, tstop + ts, ts)
 plt.plot(t, data, '-*')
